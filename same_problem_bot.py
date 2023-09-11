c8ac9850f64485eccb84b94c08ed5e1a5f08b969
CoconutMacaroon <45187468+CoconutMacaroon@users.noreply.github.com>
2023-09-10 19:15:33 -0700

    Fix bugs, prepare bot for production

--- same_problem_bot.py
+++ same_problem_bot.py
@@ -15,21 +15,23 @@ load_dotenv()
 #
 # SCAN_DELAY must be divisible by len(SITES)
 # SCAN_DELAY/len(SITES) must be greater than or equal to 60 seconds
-SCAN_DELAY: int = 180
+SCAN_DELAY: int = 120
 
 # the list of sites to be monitored
 SITES: list = ["stackoverflow"]
 
 # bypass the filters, post all comments to chat
 # this is useful for testing
-BYPASS_FILTERS: bool = True
+BYPASS_FILTERS: bool = False
 
 # ensure SCAN_DELAY is valid
-if not(SCAN_DELAY % len(SITES) == 0):
+if not (SCAN_DELAY % len(SITES) == 0):
     raise ValueError("SCAN_DELAY must be divisible by len(SITES).")
 
-if not(SCAN_DELAY / len(SITES) >= 60):
-    raise ValueError("SCAN_DELAY/len(SITES) must be greater than or equal to 60 seconds.")
+if not (SCAN_DELAY / len(SITES) >= 60):
+    raise ValueError(
+        "SCAN_DELAY/len(SITES) must be greater than or equal to 60 seconds."
+    )
 
 # create the API objects
 APIs: List[StackAPI] = [StackAPI(site, key=getenv("API_KEY")) for site in SITES]
@@ -42,6 +44,7 @@ chat_room: rooms.Room = chat_client.get_room(getenv("CHAT_ROOM_ID"))
 chat_room.send_message("[Same Problem Bot] Bot started.")
 chat_client.login(getenv("CHAT_EMAIL"), getenv("CHAT_PASSWORD"))
 
+
 def truncate_string(text: str, length: int) -> str:
     """
     Truncates a string to a given length.
@@ -49,7 +52,7 @@ def truncate_string(text: str, length: int) -> str:
     if len(text) <= length:
         return text
     else:
-        return text[:(length - 3)] + "..."
+        return text[: (length - 3)] + "..."
 
 
 def seconds_since_epoch() -> int:
@@ -73,18 +76,22 @@ def check_comment(comment: str) -> bool:
     """
     if BYPASS_FILTERS:
         return True
-    
+
     if ("same problem" in comment.lower()) and (len(comment) < 45):
         return True
     return False
+
+
+last_query: int = seconds_since_epoch()
 
 
 def scan_site(site: StackAPI) -> None:
     """
     Scans a site for "same problem" comments.
     """
-    sleep_progress(SCAN_DELAY / len(SITES))
-    
+    global last_query
+    sleep_progress(SCAN_DELAY // len(SITES))
+
     current_time = seconds_since_epoch()
     comments = site.fetch(
         "comments",
@@ -92,18 +99,17 @@ def scan_site(site: StackAPI) -> None:
         todate=current_time,
         fromdate=last_query,
     )
-    
+
     for comment in comments["items"]:
         if comment["owner"]["reputation"] < 50:
             continue
         if check_comment(comment["body"]):
-            chat_room.send_message(f"[Same Problem Bot] Potentially bad comment detected '{truncate_string(comment['body'], 100)}'")
-    
+            chat_room.send_message(
+                f"[Same Problem Bot] Potentially bad comment detected '{truncate_string(comment['body'], 100)}', link: {comment['link']}"
+            )
+
     last_query = current_time
-    
 
-
-last_query: int = seconds_since_epoch()
 
 while True:
     for site in APIs:
